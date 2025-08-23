53764dbc43ff2e64b7999d98ff76bbb229913f65
Hans Harold Lee Flores <125562851+HansongProgramming@users.noreply.github.com>
2025-08-23 18:54:51 +0800

    button script bug fixed

--- main.py
+++ main.py
@@ -15,22 +15,6 @@ async def home():
         html_content = f.read()
     return HTMLResponse(content=html_content, status_code=200)
 
-@app.get("/video")
-async def proxy_video():
-    try:
-        async with httpx.AsyncClient(follow_redirects=True, timeout=None) as client:
-            r = await client.get(DROPBOX_URL, timeout=None)
-            r.raise_for_status()  # raises exception for non-200
-            return StreamingResponse(
-                r.aiter_bytes(),
-                media_type="video/mp4",
-                headers={"Access-Control-Allow-Origin": "*"}
-            )
-    except httpx.HTTPStatusError as e:
-        return {"error": f"Bad status from Dropbox: {e.response.status_code}"}
-    except Exception as e:
-        return {"error": str(e)}
-
 @app.get("/landing", response_class=HTMLResponse)
 async def landing():
     with open("templates/Augmented-reality-landing.html", "r", encoding="utf-8") as f:
